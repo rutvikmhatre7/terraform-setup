2a21bba891dc68fd14ea063115ee0a64f765462a
rutvikmhatre7 <rutvikmhatrebusi@gmail.com>
2025-06-05 16:13:38 +0530

    adding secrets

--- providers.tf
+++ providers.tf
@@ -20,4 +20,10 @@ terraform {
     }
   }
 }
-
+provider "azurerm" {
+  features {}
+  subscription_id   = "${{ secrets.SUBSCRIPTION_ID }}"
+  tenant_id         = "${{ secrets.TENANT_ID }}"
+  client_id         = "${{ secrets.CLIENT_ID }}"
+  client_secret     = "${{ secrets.CLIENT_SECRET }}"
+}
