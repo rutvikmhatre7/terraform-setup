322e78ebed1ad4740a9f83ee2103eadc1ec225a9
rutvikmhatre7 <rutvikmhatrebusi@gmail.com>
2025-06-05 17:08:45 +0530

    adding terafform envs

--- providers.tf
+++ providers.tf
@@ -22,8 +22,5 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id   = "${{ secrets.SUBSCRIPTION_ID }}"
-  tenant_id         = "${{ secrets.TENANT_ID }}"
-  client_id         = "${{ secrets.CLIENT_ID }}"
-  client_secret     = "${{ secrets.CLIENT_SECRET }}"
+
 }
